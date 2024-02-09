b9c52f55a79aabe9c6af3b62dc9106d239a93368
ridafatima7 <rida.bsse3962@iiu.edu.pk>
2024-02-09 11:57:53 +0500

    roomph commit

--- src/Components/RNavbar.js
+++ src/Components/RNavbar.js
@@ -1,31 +1,19 @@
 import React from 'react'
-// import Logo from './Assets/designer.png'
+import  './Files/Romph.css';
+import logo from './Files/logo.png';
 // import {Link} from 'react-router-dom'
 import{Input,Container, Navbar,Nav, Button,NavItem,NavLink, NavbarBrand,Image } from 'reactstrap';
 const RNavbar = () => {
   return (
     <>
-     <Navbar bg="light" expand="lg" style={{background:"#d9f2e4"}}>
-      <Container fluid >
-      <Nav className="mr-auto" navbar bg="light"> 
-           <NavbarBrand href="/"><img src={Logo} alt="Logo" fluid /></NavbarBrand> 
-           <NavItem style={{ marginLeft: '320px', fontFamily: 'Poppins' }} className='pt-2'>
-           <Link to="/about" className="nav-link">
-               About Us
-           </Link>
-           </NavItem>
-           <NavItem className='pt-2' style={{ fontFamily: 'Poppins' }}>
-            <Link to="/FAQs" className="nav-link">
-                Ordinaryoucs
-            </Link>
-         </NavItem>
-                <NavItem className='pt-2' style={{fontFamily:'Poppins'}} > 
-                    <NavLink href="#">FAQs</NavLink> 
-                </NavItem> 
-                <NavItem className='pt-2' style={{ fontFamily:'Poppins'}} > 
-                <Button variant="outline-white" style={{ background: 'white',border:'white', borderRadius: '0' ,color: 'black', marginLeft: '320px' }}>Connect Wallet</Button>
-                </NavItem> 
-          </Nav> 
+     <Navbar expand="lg">
+      <Container fluid style={{  display: 'flex'}}>
+      <Nav className="mr-auto" navbar style={{  display: 'flex'}} > 
+        <NavbarBrand href="/"><img src={logo} alt="Logo" fluid /></NavbarBrand> 
+            <NavItem className='pt-2' style={{  display: 'flex',fontFamily:'Poppins'}} > 
+              <Button  style={{background:'white', border:'white', outline: 'none', borderRadius: '0' ,color: 'black', marginLeft: '1090px',color: "#ff3d00"}}>Sign Up</Button>
+            </NavItem> 
+       </Nav> 
       </Container>
     </Navbar>
     </>
